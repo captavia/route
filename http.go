7c747d08d1870755f7f907e9393f8729cce2a568
captavia <captavia@outlook.com>
2024-12-04 15:24:26 +0800

    init

---
+++ http.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+type HttpMux struct {
+	router *Router[*httpContext]
+	pool   *sync.Pool
+}
+
+func NewHttpMux() *HttpMux {
+	return &HttpMux{
+		router: NewRouter[*httpContext](),
+		pool: &sync.Pool{
+			New: func() interface{} {
+				return new(httpContext)
+			},
+		},
+	}
+}
+
+func (c *HttpMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := c.pool.Get().(*httpContext)
+	c.router.Serve(r.RequestURI, func() *httpContext {
+		ctx.w = w
+		ctx.r = r
+		return ctx
+	})
+	c.pool.Put(ctx)
+}
+
+type httpContext struct {
+	w http.ResponseWriter
+	r *http.Request
+}
+
+func (c *httpContext) NotMatch() {
+	http.NotFound(c.w, c.r)
+}
